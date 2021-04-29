c9b13bf4145cd98bbbb12488f6167430e27e350e
shraddha.bhalsing <shraddha.bhalsing@gmail.com>
2021-04-29 23:34:59 +0200

    Automation task

---
+++ src/main/java/com/qa/page/AboutYouMainPage.java
@@ -0,0 +1,340 @@
+package com.qa.page;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.asserts.SoftAssert;
+import com.qa.base.Base;
+
+public class AboutYouMainPage extends Base {
+
+	SoftAssert sofassrt=new SoftAssert();
+	
+	// Find elements at run time
+
+	@FindBy(xpath="//button[contains(text(),'Ok')]")
+	WebElement cookiebtn;
+
+	@FindBy(xpath="//li[@data-test-id='UserAccount']")
+	WebElement userAccountBtn;
+
+	@FindBy(xpath="//div[@class='sc-fzXfMB ccpTSg euli1-0 gFzyhh']//div[contains(text(),'Registrieren')]")
+	WebElement registerBtn;
+
+	@FindBy(xpath="//input[@name='firstName']")
+	WebElement firstNameTextField;
+
+	@FindBy(xpath="//input[@name='lastName']")
+	WebElement lastNameTextField;
+
+	@FindBy(xpath="//input[@name='email']")
+	WebElement emailTextField;
+
+	@FindBy(xpath="//input[@name='password']")
+	WebElement passwordTextField;
+	
+	@FindBy(xpath="//input[@value='female']")
+	WebElement salutationRadioBtn;
+
+	@FindBy(xpath="//button[@data-testid='RegisterSubmitButton']")
+	WebElement joinNowBtn;
+
+	@FindBy(xpath="//button[contains(text(),'Abmelden')]")
+	WebElement signOutBtn;
+
+	@FindBy(xpath="//font[contains(text(),'//div[contains(text(),'Mit anderem Konto anmelden')]")
+	WebElement differentAccSgninBtn;
+
+	@FindBy(xpath="//button[@type='submit']")
+	WebElement submitBtn;
+
+	@FindBy(xpath="//a[@title='Kinder']")
+	WebElement kinderTab;
+
+	@FindBy(xpath="//section[@class='gi00w3-0 sc-19tq43e-2 kqfkWF sc-1qdczr2-0 kLBmzm']//span[contains(text(),'Babys')]")
+	WebElement babysLink;
+
+	@FindBy(xpath="//span[contains(text(),'Bekleidung')]")
+	WebElement babysBekleidungLink;
+
+	@FindBy(xpath="//span[contains(text(),'Jacken & Mäntel')]")
+	WebElement JackenAndMantelLink;
+
+	@FindBy(xpath="//button[contains(text(),'Größe')]")
+	WebElement sizedrpdn;
+
+	@FindBys(@FindBy(xpath="//div[@class='sc-1bc31mp-0 XPlmn']"))
+	List<WebElement> sizetext;
+
+	@FindBys(@FindBy(xpath="//div[@class='sc-1bc31mp-0 XPlmn']//label"))
+	List<WebElement> sizecheckBox;
+
+
+	@FindBy(xpath="//button[@class='sc-1wzrtji-0 hPfBHn']//div[contains(text(),'Sortierung')]")
+	WebElement sortSelection;
+
+	@FindBy(xpath="//div[contains(text(),'Niedrigster Preis')]")
+	WebElement sortSelectionVal;
+
+	@FindBy(xpath="//button[contains(text(),'Hinzufügen')]")
+	WebElement addToCartBtn;
+
+	@FindBy(xpath="//div[@class='sectionWrapper--3ioAR']//button[contains(text(),'Zur Kasse')]")
+	WebElement chkOutBtn;
+
+	@FindBy(xpath="//button//span[contains(text(),'Jetzt zahlungspflichtig bestellen')]")
+	WebElement  orderNowBtn;
+
+
+	@FindBy(xpath="//div[@class='sc-1mm0dd3-3 eyXQwH']//div[@class='sc-1oa7xla-2 sc-11k7naj-2 nAsZs']//preceding::input[@type='checkbox']")
+	WebElement  chkbxbtn;
+
+
+	@FindBys(@FindBy(xpath="//div[@class='sc-1mm0dd3-3 eyXQwH']//div[@class='sc-1oa7xla-2 sc-11k7naj-2 nAsZs']"))
+	List<WebElement>  clicksiz;
+
+
+	//Initialize WebElements mentioned in the current page
+	public void initializeWebElement(){
+		PageFactory.initElements(driver, this);
+	}
+
+	//Method to accept cookie pop up and login into application
+	public String login(String firstName,String emailId,String password){
+		try
+		{
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			if (cookiebtn.isDisplayed())
+			{
+				cookiebtn.click();
+			}
+
+			userAccountBtn.click();
+			emailTextField.sendKeys(emailId);
+			passwordTextField.sendKeys(password);
+			submitBtn.click();
+
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		WebElement verifyFirstNameOnPage=driver.findElement(By.xpath("//span[contains(text(),'"+firstName+"')]"));
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return verifyFirstNameOnPage.getText();
+	}
+
+	//Method to select category as Children and their clothing type
+	public String selectCategory(){
+
+		WebDriverWait wait2 = new WebDriverWait(driver, 20);
+		wait2.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@title='Kinder']")));
+		try
+		{
+			kinderTab.click();
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		babysLink.click();
+		babysBekleidungLink.click();
+		JackenAndMantelLink.click();	
+
+		WebElement clothType=driver.findElement(By.xpath("//span[contains(text(),'Babyjacken')]"));
+		return clothType.getText();
+	}
+
+	//Method to set filter on clothing size
+
+	public Boolean setFilterOnSize(String sizeVal)
+	{
+
+		sizedrpdn.click();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try
+		{
+			for (int i=0;i<clicksiz.size();i++)
+			{
+
+				String actualSize=clicksiz.get(i).getText();
+				if (sizeVal.equals(actualSize))
+				{
+					driver.findElement(By.xpath("//div[@class='sc-1mm0dd3-3 eyXQwH']//div[@class='sc-1oa7xla-2 sc-11k7naj-2 nAsZs']//preceding::input[@type='checkbox']")).click();
+				
+					break;
+				}
+			}
+
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return sortSelection.isDisplayed();
+
+	}
+
+// Method to select respective clothing from the list
+	public Boolean selectProduct(String expectedProductId)
+	{
+		sizedrpdn.click();
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try
+		{
+			List<WebElement>lst1=driver.findElements(By.xpath("//div[@class='sc-19tq43e-1 iRbhuv']//div[5]//div[@class='sc-1n50fuf-0 jLIxQJ']//a"));
+			String actualProductId = null;
+			for (int j=0;j<lst1.size();j++)
+			{
+				actualProductId=lst1.get(j).getAttribute("id");
+
+
+				if (expectedProductId.equals(actualProductId))
+				{
+					driver.findElement(By.xpath("//div[@class='sc-19tq43e-1 iRbhuv']//div[5]//div[@class='sc-1n50fuf-0 jLIxQJ']//a//img['"+j+"']")).click();
+					break;
+				}
+			}
+
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return addToCartBtn.isEnabled();
+
+	}
+	
+	//Method to select size and add product to the cart
+
+	public Boolean addProductToCart(String expectedClothSize)
+	{
+		try
+		{
+			List<WebElement>selectSizeOfCloth=driver.findElements(By.xpath("//div[@class='sc-1wjc9re-1 kpeKIf']//button//div"));
+
+			for (int i=0;i<selectSizeOfCloth.size();i++)
+			{
+
+				String actualClothSize = selectSizeOfCloth.get(i).getText();
+
+				if (expectedClothSize.equals(actualClothSize))
+				{
+					driver.findElement(By.xpath("//div[@class='sc-1wjc9re-1 kpeKIf']//button['"+i+"']")).click();
+     			}
+			}
+
+			addToCartBtn.click();
+
+		}catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return chkOutBtn.isEnabled();
+	}
+
+	//Method to checkOut the product
+	public String clickCheckOut()
+	{
+		Boolean checkOutBtnStatus=chkOutBtn.isEnabled();
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		if (checkOutBtnStatus)
+		{
+			js.executeScript("arguments[0].click();", chkOutBtn);
+		}
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return driver.getTitle();
+	}
+
+	//Method to select card type
+	public String selectCardForPayment(String expectedCardType)
+	{
+		//	String val1="computop_creditcard_mastercard";
+		String actualCardType = null;
+
+		List<WebElement>ls=driver.findElements(By.xpath("//div[@class='payment-selection-list compact']//ul//input"));
+
+		for (int i=0;i<ls.size();i++)
+		{
+
+			actualCardType=ls.get(i).getAttribute("value");
+			JavascriptExecutor js1=(JavascriptExecutor)driver;
+
+			if (expectedCardType.equals(actualCardType))
+			{
+
+				js1.executeScript("arguments[0].click();", ls.get(i));
+				break;
+			}
+		}
+
+		return actualCardType;
+	}
+
+	//Method to Order product and verify Payment card details pop up  appear
+	public Boolean orderNow()
+	{
+		JavascriptExecutor js2=(JavascriptExecutor)driver;
+		js2.executeScript("window.scrollBy(0,1000)");
+		boolean orderNowBtnStatus=orderNowBtn.isEnabled();
+		WebDriverWait waitForCardDetailspopupWindow = new WebDriverWait(driver, 20);
+		waitForCardDetailspopupWindow.until(ExpectedConditions.elementToBeClickable(By.xpath("//button//span[contains(text(),'Jetzt zahlungspflichtig bestellen')]")));
+		if (orderNowBtnStatus)
+		{
+			js2.executeScript("arguments[0].click();", orderNowBtn);
+		}
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return driver.findElement(By.xpath("//div[@class='modal-container']")).isDisplayed();
+	
+	}
+
+}
---
+++ src/test/java/com/qa/testcases/AboutYouMainPageTest.java
@@ -0,0 +1,254 @@
+package com.qa.testcases;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.qa.base.Base;
+import com.qa.page.AboutYouMainPage;
+import com.qa.util.CommonUtil;
+
+public class AboutYouMainPageTest extends Base {
+
+	//Create Object of MainPage where OR and methods are maintained
+	AboutYouMainPage mainPageObj=new AboutYouMainPage();
+
+
+	public static ExtentHtmlReporter pathHtml;
+	public static ExtentReports exReport;
+	public static ExtentTest exLog,exLog1,exLog2,exLog3,exLog4,exLog5,exLog6,exLog7,exLog8,exLog9;
+
+
+
+	//Prerequisite steps to be executed prior executing actual TestCases
+
+	@BeforeTest
+	public void basicSetUp()
+	{
+		DriversetUp();
+		pathHtml=new ExtentHtmlReporter(System.getProperty("user.dir")+prop.getProperty("ReportPath"));
+		exReport=new ExtentReports();
+		exReport.attachReporter(pathHtml);
+
+	}
+	
+	
+	// Launch URL TestCase
+
+	@Test(priority=1)
+	public void launchURL(){
+		String pageTitle=LaunchBrowser();
+		mainPageObj.initializeWebElement();
+		exLog=exReport.createTest("Verify user can launch URL", "Automation");
+		if (pageTitle.equals("Mode online von mehr als 2.000 Top-Marken | ABOUT YOU"))
+		{
+			exLog.log(Status.PASS,"URL is launched successfully") ;
+		}
+
+		else
+
+		{
+			exLog.log(Status.FAIL,"Failed to launch URL") ;
+		}
+	}
+
+
+	@DataProvider
+	public Object[][] passSheet()
+	{
+		Object[][] val=CommonUtil.readDataFromExcel("InputData");
+		return val;
+	}
+
+
+
+	// TestCase to verify user is able to Login in application
+
+	@Test(priority=2,dataProvider="passSheet")
+	//	@Parameters({"firstName","emailId","password"})
+	public void clickFirstProductAndEnterEmail(String firstName,String emailId,String password){
+		String verifyFirstNameOnPage=mainPageObj.login(firstName,emailId,password);
+		exLog2=exReport.createTest("Verify user is able to Login in application","Automation");
+		if (verifyFirstNameOnPage.equalsIgnoreCase(firstName))
+		{
+			exLog2.log(Status.PASS,"User is able to login into WebApp") ;
+		}
+		else
+
+		{
+			exLog2.log(Status.FAIL,"User failed to login into WebApp") ;
+		}
+
+
+
+	}
+
+	//Testcase to verify user is able to select the Children category as Babies and cloth category as Jacket
+
+	@Test(priority=3)
+	public void selectBabysClothTypeCategory(){
+		String verClothSelected= mainPageObj.selectCategory();
+		exLog3=exReport.createTest("Verify user is able to select the Children category as Babies and cloth category as Jacket" , "Automation");
+		if (verClothSelected.contains("Babyjacken"))
+		{
+			exLog3.log(Status.PASS,"User is able to select the Children category as Babies ,cloth category as Jacket successfully ") ;
+		}
+
+		else
+
+		{
+			exLog3.log(Status.FAIL," Failed to select the Children category as Babies ,cloth category as Jacket") ;
+		}
+	}
+
+	//Testcase to verify user is able to apply filter on size
+
+	@Test(priority=4)
+	@Parameters({"sizeVal"})
+	public void filterForSize(String sizeVal){
+
+
+		Boolean verifySizeSelected=mainPageObj.setFilterOnSize(sizeVal);
+
+		exLog4=exReport.createTest("Verify user is able to apply filter on size ", "Automation");
+		if (verifySizeSelected)
+		{
+			exLog4.log(Status.PASS,"Filter applied on size") ;
+		}
+		else
+
+		{
+			exLog4.log(Status.FAIL," Failed to apply filter on size ") ;
+
+		}
+
+	}
+
+    //Testcase to verify user is able to select product
+	
+	@Test(priority=5)
+	@Parameters({"expectedProductId"})
+	public void selectProduct(String expectedProductId )
+	{
+
+		Boolean verifyAddToCartbtnEnabled= mainPageObj.selectProduct(expectedProductId);
+
+
+		exLog5=exReport.createTest("Verify user is able to select product", "Automation");
+		if (verifyAddToCartbtnEnabled)
+		{
+			exLog5.log(Status.PASS,"User is able to select product ") ;
+		}
+		else
+
+		{
+			exLog5.log(Status.FAIL," User failed to select product ") ;
+
+		}
+
+	}
+
+  //Testcase to verify user is able to select size for respective product
+
+	@Test(priority=6)
+	@Parameters({"expectedSize"})
+	public void selectSizeAndaddProductToCart(String expectedSize)
+	{
+
+		Boolean verifyCheckOutbtnEnabled= mainPageObj.addProductToCart(expectedSize);
+
+
+		exLog6=exReport.createTest("Verify user is able to select size for respective product", "Automation");
+		if (verifyCheckOutbtnEnabled)
+		{
+			exLog6.log(Status.PASS,"User is able to select size for product ") ;
+		}
+		else
+
+		{
+			exLog6.log(Status.FAIL," User failed to select size for product ") ;
+
+		}
+	}
+
+	//Testcase to verify user is able to click on Checkout button
+
+	@Test(priority=7)
+	public void checkOutProduct()
+	{
+
+		String verifyAfterCheckOutPagetitle= mainPageObj.clickCheckOut();
+
+
+		exLog7=exReport.createTest("Verify user is able to click on Checkout button", "Automation");
+		if (verifyAfterCheckOutPagetitle.equals("Sicher und einfach bei About You bestellen."))
+		{
+			exLog7.log(Status.PASS,"User is able to checkOut the product") ;
+		}
+		else
+
+		{
+			exLog7.log(Status.FAIL," User failed to checkOut the product") ;
+
+		}
+
+	}
+
+	//Testcase to verify user is able to select card type
+	@Test(priority=8)
+	@Parameters({"expectedCardType"})
+	public void cardTypeForPayment(String expectedCardType){
+		String verifyactualCardType=mainPageObj.selectCardForPayment(expectedCardType);
+		exLog8=exReport.createTest("Verify user is able to select card type","Automation");
+		if (verifyactualCardType.equals(expectedCardType))
+		{
+			exLog8.log(Status.PASS,"User is able to select Card Type ") ;
+		}
+		else
+
+		{
+			exLog8.log(Status.FAIL,"User failed to select card type") ;
+		}
+
+
+
+	}
+
+	//Testcase to verify card detail pop up appears
+
+	@Test(priority=9)
+
+	public void cardTypeForPayment(){
+		Boolean verifyCardDetailsPopUp=mainPageObj.orderNow();
+		exLog9=exReport.createTest("Verify card detail pop up appeared ","Automation");
+		if (verifyCardDetailsPopUp)
+		{
+			exLog9.log(Status.PASS,"User is able to see the card detail pop up window ") ;
+		}
+		else
+
+		{
+			exLog9.log(Status.FAIL,"User failed to see the card detail pop up window  ") ;
+		}
+
+
+
+	}
+
+
+	//Close browser
+
+	@AfterTest
+	public void closeBrowser() {
+		exReport.flush();
+	    driver.close();
+
+
+
+	}
+}
